5004edb4ca799d76175ba306d0d8b2335be0824e
OpenShift guest <two.pelicans@mac.com>
2013-07-03 09:22:39 -0400

    Added some more files ammended other and added to the Readme

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 Presenter
 =========
 
-Another Impress.js, Websockets, Presenter Service
+Hopefully delivering a Remote controlled presentation solution using
+Impress.j as the presentation framework with Python (Flask, Server Sent Events)
+for control.
+SSEs are implemented in all recent browsers less IE.  for older browsers
+you can use pollyfills eg eventsource.js or jquery.eventsource.
+SSE was chosen over Web Sockets to KISS as I can use std Http for the presenter 
+and slide changes pushed to the clients via SSE.  all in one app and the standard
+http ports which for me is key
--- server.py
+++ server.py
@@ -1,18 +1,63 @@
-from flask import Flask
-import os
+from flask import Flask, request, Response, render_template
+import os # needed for cloud9
+
+# might want to use gevent for production (.monkey, .pywsgi)
+#import gevent
+#import gevent.monkey
+#from gevent.pywsgi import WSGIServer
+#gevent.monkey.patch_all()
 
 app = Flask(__name__)
 port = os.environ["PORT"] # Needed for running on Cloud 9
 ip = os.environ["IP"]     # Needed for running on Cloud 9
 
+# Helper Functions
 
-@app.route('/')
+def audience_stream(): 
+    #SSE message format is 'data: <Content> \n\n'
+    content = 'Test'
+    return "data:  %s \n\n " % content
+    
+# end of helper functions
+
+@app.route('/config')
 def working():
+    # ToDo need to add a browser check
     return "Flask up and running on http//" + ip + " : " + port
+
+
+# Lists the available presentations
 
 @app.route('/presentations')
 def presentations():
     return "Presentations Folder"
+    
+#presenter selects presentation from presentations folder
+#presenter tells '/' which presention.html file to send to browser. 
+#presenter.html sends the navigation messages via XHR
+
+@app.route('/presenter')
+def presenter():
+    return render_template('presenter.html') # needs to be WinPhone 7 compliant
+    
+# provides the presentation the URL to listen out on    
+@app.route('/audience')
+def sse_request():
+    return Response(
+            audience_stream(),
+            mimetype='text/event-stream')
+            
+
+#The members of the Audience simply www.xx.xx/ to access the presentation
+@app.route('/')
+def page():
+    return render_template('presentation.html') # will need polyfills for older browsers and IE<10
+
 
 if __name__=='__main__':
     app.run(host=ip, port=port) #ip and port required for Cloud 9
+    
+# If using gevent
+#if __name__ == '__main__':
+#    http_server = WSGIServer(('127.0.0.1', 8001), app)
+#    http_server.serve_forever()
